434668fb5cb5ccf77eed8897a6fc7ae9393d802b
Donaldwilonc <donaldwilsonc@gmail.com>
2026-05-07 19:23:01 +0100

    Add files via upload

--- test.sh
+++ test.sh
@@ -10,18 +10,25 @@ BLUE='\e[34m'
 
 # ------------------------------------------------------------------Main Code--------------------------------------------------------------
 ping -c 4 www.google.com > /dev/null
-if [[ $? -eq 0 ]]; then
+status=$?
+
+# ---Main if/else Block
+if [[ $status -eq 0 ]]; then
    printf "${GREEN}Connected to the internet.${NC}\n"
-elif [[ $? -ne 0 ]]; then
+elif [[ $status -ne 0 ]]; then
    printf "${RED}Cannot reach the internet, do you want to connect to a network(y/n).${NC}"
    read -p "" response
-   if [[ response -eq 'y' ]]; then
+   response="${response,,}"
+   
+#---Decision making with string inputs--- 
+   if [[ $response == 'y' ]]; then
       nmcli device wifi list
       read -p "Enter your SSID: " ssid
-      sudo nmcli device wifi connect ${ssid}
-   elif [[ response -eq 'n' ]]; then
+      sudo nmcli device wifi connect "${ssid}" --ask
+   elif [[ $response == 'n' ]]; then
       printf "${YELLOW}Exiting...${NC}\n"
       exit 0
    fi
+
 fi
 
